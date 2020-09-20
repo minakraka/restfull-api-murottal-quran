5ce566d37524e674d4c90c9b5d0cd208fcb87de2
minakraka <rakanurpandi@gmail.com>
2020-09-20 20:54:16 +0700

    Setup serializers and url

--- restfullapi/murottal/models.py
+++ restfullapi/murottal/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Surat(models.Model):
+    no_surat = models.IntegerField()
+    nama_surat = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama_surat
